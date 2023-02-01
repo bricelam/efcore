8c931813860ee149e8411631292b10f1a3b49f92
Brice Lambson <brice@bricelam.net>
2023-02-01 14:56:14 -0800

    fixup! WIP

---
+++ prototypes/EFCore.VS/DbContextItemTemplate/Class.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+$
+if$ ($targetframeworkversion$ >= 3.5)using System.Linq;
+$endif$using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace $rootnamespace$
+{
+	internal class $safeitemrootname$ : DbContext
+	{
+	}
+}
---
+++ prototypes/EFCore.VS/EFCore.VS/DbContextWizard.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using EnvDTE;
+using Microsoft.VisualStudio.TemplateWizard;
+
+namespace Microsoft.EntityFrameworkCore.VisualStudio;
+
+internal class DbContextWizard : IWizard
+{
+    // Called as the template file begins to be processed.
+    public void RunStarted(
+        object automationObject,
+        Dictionary<string, string> replacementsDictionary,
+        WizardRunKind runKind,
+        object[] customParams)
+    {
+        ThreadHelper.ThrowIfNotOnUIThread();
+        Debug.Assert(runKind == WizardRunKind.AsNewItem);
+
+        var dte = (DTE)automationObject;
+
+        // TODO
+    }
+
+    public bool ShouldAddProjectItem(string filePath)
+        => false;
+
+    // Called when a project item has finished being generated.
+    public void ProjectItemFinishedGenerating(ProjectItem projectItem)
+    {
+    }
+
+    // Called before a project item is opened within the editor.
+    public void BeforeOpeningFile(ProjectItem projectItem)
+    {
+    }
+
+    public void ProjectFinishedGenerating(EnvDTE.Project project)
+        => throw new NotImplementedException();
+
+    // Called when the wizard has finished generating outputs.
+    public void RunFinished()
+    {
+    }
+}
