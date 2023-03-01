642ca505fbff6d85e9ae7025316ec5dd2be6e0a8
Brice Lambson <brice@bricelam.net>
2023-02-28 16:26:12 -0800

    fixup! WIP

--- prototypes/EFCore.VS/EFCore.VS/NewDbContextForm.cs
+++ prototypes/EFCore.VS/EFCore.VS/NewDbContextForm.cs
@@ -1,20 +1,63 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Data.Common;
 using System.Windows.Forms;
+using Microsoft.VisualStudio.Data.Core;
+using Microsoft.VisualStudio.Data.Services;
 
-namespace Microsoft.EntityFrameworkCore.VisualStudio
+namespace Microsoft.EntityFrameworkCore.VisualStudio;
+
+public partial class NewDbContextForm : Form
 {
-    public partial class NewDbContextForm : Form
+    public NewDbContextForm()
+        => InitializeComponent();
+
+    private void button1_Click(object sender, EventArgs e)
     {
-        public NewDbContextForm()
-        {
-            InitializeComponent();
-        }
+        ThreadHelper.ThrowIfNotOnUIThread();
+
+        var dialogFactory = (IVsDataConnectionDialogFactory)ServiceProvider.GlobalProvider.GetService(typeof(IVsDataConnectionDialogFactory));
+        var providerManager = (IVsDataProviderManager)ServiceProvider.GlobalProvider.GetService(typeof(IVsDataProviderManager));
+
+        var dialog = dialogFactory.CreateConnectionDialog();
+        dialog.AddSources(
+            (_, providerKey) =>
+            {
+                var provider = providerManager.Providers[providerKey];
+                if (provider.Technology != new Guid("77AB9A9D-78B9-4BA7-91AC-873F5338F1D2"))
+                {
+                    return false;
+                }
+
+                var invariantName = provider.GetProperty("InvariantName") as string;
+                if (invariantName is null)
+                {
+                    return false;
+                }
+
+                DbProviderFactory providerFactory = null;
+                try
+                {
+                    providerFactory = DbProviderFactories.GetFactory(invariantName);
+                }
+                catch
+                {
+                }
+                if (providerFactory is null)
+                {
+                    // TODO: Can we do something with just the invariant name?
+                    return false;
+                }
+
+                string connectionType;
+                using (var connection = providerFactory.CreateConnection())
+                {
+                    connectionType = connection.GetType().FullName;
+                }
+
+                // TODO: Match to an EF provider
+                return true;
+            });
+        dialog.ShowDialog();
+
+        label1.Text = dialog.SafeConnectionString;
     }
 }
---
+++ prototypes/EFCore.VS/EFCore.VS/ProviderRegistry.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+
+namespace Microsoft.EntityFrameworkCore.VisualStudio;
+
+internal static class ProviderRegistry
+{
+    private static readonly (string name, Guid VsDataProvider)[] _providers = new[]
+    {
+        ("Microsoft.EntityFrameworkCore.SqlServer", new Guid("8800600A-ADD9-47E8-81D2-1D13B5A09C13")),
+        ("Microsoft.EntityFrameworkCore.Sqlite", new Guid("796A79E8-2579-4375-9E12-03A9E0D1FC02")),
+        ("Npgsql.EntityFrameworkCore.PostgreSQL", new Guid("70ba90f8-3027-4aF1-9b15-37abbd48744c")),
+        ("Oracle.EntityFrameworkCore", new Guid("9D8FDBB9-EE60-4787-B7AE-49831D34AD4B")),
+
+        // TODO: DevArt
+
+        // TODO: No DDEX provider
+        //IBM.EntityFrameworkCore
+        //MySql.EntityFrameworkCore
+        //FirebirdSql.EntityFrameworkCore.Firebird
+        //Pomelo.EntityFrameworkCore.MySql
+        //EntityFrameworkCore.Jet
+        //Google.Cloud.EntityFrameworkCore.Spanner
+    };
+
+    public static bool IsVsDataProviderSupported(Guid provider)
+        => _providers.Any(p => p.VsDataProvider == provider);
+}
